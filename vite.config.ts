e65008d6ec166607607099017a1d0d4be41867bb
Nosson M Frankel <nossonmfrankel@gmail.com>
2025-02-13 20:35:46 -0500

    whitelist traffic from any host

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	server: {
+		allowedHosts: true
+	},
 	plugins: [sveltekit(), tailwindcss()]
 });
